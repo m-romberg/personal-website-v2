3ee154892acc5ec2505ac5b39777a8ea43b48c8f
Madelyn Romberg <madelynromberg@g.ucla.edu>
2023-05-11 14:32:49 -0700

    basic biography card

---
+++ src/Biography.js
@@ -0,0 +1,32 @@
+function Biography(){
+
+  return (
+    <section className="Biography">
+      <p className="Biography-paragraph">
+      I'm a full-stack developer in <b>Los Angeles, California</b> with a passion for
+      contributing to revolutionary ideas and building meaningful projects.
+      After completing two degrees at UCLA and a full stack web development
+      program, I am currently seeking work as a software engineer.
+      </p>
+      <p className="Biography-paragraph">
+      Throughout my time as a researcher in both chemistry and history,
+      I constantly leveraged the power of code to discover, clean, and convey
+      never-before-seen information that led to tangible impacts. Previously,
+      I was an atmospheric chemist at <b>NASA Jet Propulsion Laboratory</b>,
+      supporting the development of a <b>chemical reanalysis model</b>
+      to be used bypublic health officials regarding air quality standards.
+      Additionally, I had a project commissioned with the <b>Los Angeles Public
+      Library's Special Collections</b> to build a digital database that improved
+      accessibility and increased user traffick. Programming enabled me to
+      engage in two vastly different sectors and effective to make change in
+      both. Now, I'm a full time programmer.
+      </p>
+      <p className="Biography-paragraph Biography-closer">
+      Let's do something incredible together, one line at a time.
+      </p>
+    </section>
+  )
+
+}
+
+export default Biography
