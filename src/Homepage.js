c7e8e85e2c5dce00de3cf6e413e3c533b602f178
Madelyn Romberg <madelynromberg@g.ucla.edu>
2023-05-11 14:33:09 -0700

    homepage renders biocard

---
+++ src/Homepage.js
@@ -0,0 +1,14 @@
+import BioCard from './BioCard';
+
+function Homepage() {
+  return (
+    <div className='HomePage'>
+      <section className='HomePage-biography'>
+       <BioCard />
+      </section>
+    </div>
+
+  );
+}
+
+export default Homepage;
