0051ea17547e2e588a7a43c6382b66a8c68d3b60
Madelyn Romberg <madelynromberg@g.ucla.edu>
2023-05-13 17:56:41 -0700

    create github project objects

---
+++ src/githubProjects.js
@@ -0,0 +1,25 @@
+const github_p1 = {
+  name: "Job.ly Backend",
+  url: "https://github.com/m-romberg/jobly-backend",
+  description: "Backend job search application for Job.ly built using Espress.js"
+};
+
+const github_p2 = {
+  name: "Job.ly Frontend",
+  url: "https://github.com/m-romberg/jobly-frontend",
+  description: "Frontend job search application built using React."
+};
+
+const github_p3 = {
+  name: "Pix.ly Backend",
+  url: "https://github.com/m-romberg/pixly-backend",
+  description: "Backend photo edit application built using Flask"
+};
+
+const github_p4 = {
+  name: "Pix.ly Frontend",
+  url: "https://github.com/m-romberg/pixly-frontend",
+  description: "Frontend job search application built using React."
+};
+
+export { github_p1, github_p2, github_p3, github_p4 };
