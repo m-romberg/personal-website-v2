eeffd049bc5f850bb0e2bec39645733a9d741bdd
Madelyn Romberg <madelynromberg@g.ucla.edu>
2023-05-15 19:08:22 -0700

    update project descriptions

--- src/githubProjects.js
+++ src/githubProjects.js
@@ -19,7 +19,7 @@ const github_p3 = {
 const github_p4 = {
   name: "Pix.ly Frontend",
   url: "https://github.com/m-romberg/pixly-frontend",
-  description: "Frontend photo application built using React."
+  description: "Frontend photo edit application built using React."
 };
 
 const projects = [ github_p1, github_p2, github_p3, github_p4 ];
